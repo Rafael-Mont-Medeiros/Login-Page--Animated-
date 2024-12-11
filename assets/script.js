a9b26088269711f407f2f41522365e6ca3af3122
Rafael-Mont-Medeiros <rafak1212@gmail.com>
2024-12-10 22:19:02 -0300

    Login page with animation.

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,17 @@ function singUpBtn() {
 function singInBtn(){
     body.classList = 'sing-in-js';
 }
+
+/* <i class="fa-regular fa-eye-slash"></i> */
+let firstPassword = document.querySelector('.first-password');
+let firistEye = document.querySelector('#first-eye');
+
+
+firistEye.addEventListener('click', ()=>{
+    const type = firstPassword.type === 'password' ? 'text' : 'password';
+    firstPassword.type = type;
+
+    this.classList.toggle("fa-eye");
+    this.classList.toggle("fa-eye-slash");
+})
+

e6a80cb39404a56487997b696899c4125de17599
Rafael-Mont-Medeiros <rafak1212@gmail.com>
2024-12-14 09:57:39 -0300

    Login animated...adicionado função de mostrar/esconder a senha.

--- assets/script.js
+++ assets/script.js
@@ -9,15 +9,27 @@ function singInBtn(){
 }
 
 /* <i class="fa-regular fa-eye-slash"></i> */
-let firstPassword = document.querySelector('.first-password');
-let firistEye = document.querySelector('#first-eye');
+let firstPassword = document.querySelector('#first-column-password');
+let firistEye = document.querySelector('#first-column-eye');
 
 
-firistEye.addEventListener('click', ()=>{
-    const type = firstPassword.type === 'password' ? 'text' : 'password';
-    firstPassword.type = type;
+firistEye.addEventListener('click',(event)=>{
+    const firstColumnPasswodrType = firstPassword.type === 'password' ? 'text' : 'password';
+    firstPassword.type = firstColumnPasswodrType;
 
-    this.classList.toggle("fa-eye");
-    this.classList.toggle("fa-eye-slash");
+    event.target.classList.toggle("fa-eye");
+    event.target.classList.toggle("fa-eye-slash");
+});
+
+
+
+let secondPassword = document.querySelector('#second-column-password');
+let secondEye = document.querySelector('#second-column-eye');
+
+secondEye.addEventListener('click', function(){
+    const secondColumnPasswodrType = secondPassword.type === 'password' ? 'text' : 'password';
+    secondPassword.type = secondColumnPasswodrType;
+
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
 })
-
